94a60bd110cfe1bf33244b126b331801205f975a
ebisLab <ebaiden@techie.com>
2020-05-20 19:05:56 -0400

    merge sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,28 +4,53 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
     # base case
-if len(arrA) == 0 or len(arrB) == 0:
-    return arrA+arrB
-elif not len(arrA) == 0 or not len(arrB) == 0:
-    # return arrA + arrB
+    a = 0
+    b = 0
+    # this is where the merging happens
 
-    a = 0  # smallest index of a
-    b = 0  # smallest index of b
-    lenA = len(arrA)
     for i in range(elements):
-        if a = lenA:
+        # if you've gone through all the values in A --> you will store the rest of B
+        if len(arrA) == a:  # read through everything in a
             merged_arr[i] = arrB[b]
+            b += 1  # incremented the index
+        elif len(arrB) == b:
+            merged_arr[i] = arrA[a]
+            a += 1
+        # compare the current values where the pointer is at
+        elif arrA[a] > arrB[b]:
+            # overriding zero with the number
+            merged_arr[i] = arrB[b]
+            b += 1
+        else:
+            # overriding zero with the number
+            merged_arr[i] = arrA[a]
+            a += 1
+        # keep track of where you're at list A and list B
+        # take the first element of each list and compare them
+            # increment the new element index
+        # take the smallest number -> assign that --> merged_arr
+        # what ever is left over take add it to the marged_arr
 
+
+# elements steps
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-    # for i in range(len(arr)):
-    # merge_sort(arr, 0, len(arr)-1)  # arra, start index, and ending index
+    # if lenght of array is greater than 1,-->
+    if len(arr) > 1:
+        # find middle
+        mid = len(arr)//2
+        # they will become 2 different arrays left, right
+        # recursive merge_sort calls over and over until we get to 1 or base statement kicks in
+        left = merge_sort(arr[0:mid])
+        right = merge_sort(arr[mid:])
+        # this is infinite until i reach one
+        # merge things in the right order until recursive stops
+        arr = merge(left, right)  # puts them in order
 
     return arr
 
